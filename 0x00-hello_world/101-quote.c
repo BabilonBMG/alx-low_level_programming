41e5824d4b433b2ad9bdcf4a5b2ec9eb7464d03d
BabilonBMG <selekea@gmail.com>
2023-07-14 01:31:13 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -12,5 +12,3 @@ int main(void)
 	"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
-/**You are not allowed to use any functions listed in the NAME section of the man (3) printf or man(3) puts 
-*/
