8a01952562c13a331f1be811e9567017e37e4d88
BabilonBMG <selekea@gmail.com>
2023-07-23 22:43:04 -0700

    ./0-isuper

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,5 +1,5 @@
-#ifndef MAIN_H
-#define MAIN_H
+#ifndef MAIN_h
+#define MAIN_h
 
 int _isupper(int c);
 int _isdigit(int c);
@@ -11,6 +11,6 @@ void print_line(int n);
 void print_diagonal(int n);
 void print_square(int size);
 void print_triangle(int size);
-int _putchar(char c)
-
+int _putchar(char c);
+void print_number(int n);
 #endif
