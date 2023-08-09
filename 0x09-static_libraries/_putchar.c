fca75ab9c9bcf7451ab17b26a612f34155eadfb5
BabilonBMG <selekea@gmail.com>
2023-08-09 04:10:13 -0700

    nm libmy.a

--- 0x09-static_libraries/_putchar.c
+++ 0x09-static_libraries/_putchar.c
@@ -1,17 +1,12 @@
-#include "main.h"
 #include <unistd.h>
-
 /**
- *_putchar - writes a character to stdout
+ * _putchar - writes the character c to stdout
  * @c: The character to print
  *
- * Return: 1 on Success
- * retrun -1 on error, and errno is set apropriately
- *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
  */
-
 int _putchar(char c)
 {
-
 	return (write(1, &c, 1));
 }
