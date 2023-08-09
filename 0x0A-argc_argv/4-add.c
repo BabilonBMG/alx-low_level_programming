8e500af7ba0b3608e1a211dc972d6b8ef3556c25
BabilonBMG <selekea@gmail.com>
2023-08-09 10:03:27 -0700

    new update

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 #include <ctype.h>
 #include <string.h>
-	
+
 /**
  * check_num - check - string there are digit
  * @str: array str
@@ -14,12 +14,12 @@ int check_num(char *str)
 		unsigned int count;
 
 		count = 0;
-		while (count < strlen(str)) 
+		while (count < strlen(str))
 
 		{
-			if (!isdigit(str[count])) 
+			if (!isdigit(str[count]))
 			{
-				return (0);
+			return (0);
 			}
 			count++;
 		}
@@ -41,7 +41,7 @@ int main(int argc, char *argv[])
 		int sum = 0;
 
 		count = 1;
-		while (count < argc) 
+		while (count < argc)
 		{
 			if (check_num(argv[count]))
 			{
