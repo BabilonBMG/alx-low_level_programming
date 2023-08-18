e59286d97eff0b1372b23457821575f1f6d4f084
BabilonBMG <selekea@gmail.com>
2023-08-17 20:56:58 -0700

    Static libraries

--- 0x09-static_libraries/3-islower.c
+++ 0x09-static_libraries/3-islower.c
@@ -1,16 +1,14 @@
 #include "main.h"
 
 /**
- * _islower - check if char is a lowercase
- * @c: the character to be checked
- * Return: ! if char is lowercase, otherwise 0.
+ * _islower - prints 1 or 0 depending on input
+ * @c: first parameter
+ *
+ * Description: prints all lowercase letters
+ * Return: Always(0).
  */
 
 int _islower(int c)
 {
-
-	if (c >= 'a' && c <= 'z')
-		return (1);
-	else
-		return (0);
+	return (c >= 'a' && c <= 'z');
 }
--- 0x09-static_libraries/5-strstr.c
+++ 0x09-static_libraries/5-strstr.c
@@ -1,32 +1,31 @@
 #include "main.h"
 
 /**
- * _strtr - Entry point
- * @haystack: input
- * @needle: input
- * Return: Always 0 (Success)
- */
-
-char *_strtr(char *haystack, char *needle)
-
+ * _strstr - finds the first occurrence of the substring.
+ * needle in the string haystack.
+ * @haystack: entire string.
+ * @needle: substring.
+ * Return: pointer to the beginning of located substring or
+ * NULL if the substring is not found.
+*/
+char *_strstr(char *haystack, char *needle)
 {
+	char *bhaystack;
+	char *pneedle;
 
-	for (; *haystack != '\0'; haystack++)
+	while (*haystack != '\0')
 	{
+		bhaystack = haystack;
+		pneedle = needle;
 
-		char *l = haystack;
-		char *p = needle;
-
-		while (*l == *p && *p != '\0')
+		while (*haystack != '\0' && *pneedle != '\0' && *haystack == *pneedle)
 		{
-
-			l++;
-			p++;
+			haystack++;
+			pneedle++;
 		}
-
-		if (*p == '\0')
-			return (haystack);
+		if (!*pneedle)
+			return (bhaystack);
+		haystack = bhaystack + 1;
 	}
-
 	return (0);
 }
