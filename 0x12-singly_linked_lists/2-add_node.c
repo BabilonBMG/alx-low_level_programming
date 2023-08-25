9b6e7d1017428662dd31b43270ef6eb26617f0bf
BabilonBMG <selekea@gmail.com>
2023-08-25 03:03:06 -0700

    ./c

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -15,15 +15,15 @@
 list_t *add_node(list_t **head, const char *str)
 {
 
-	list_t *new
+	list_t *new;
 	unsigned int len = 0;
 
 	while (str[len])
 		len++;
 	new = malloc(sizeof(list_t));
-	id(!new)
+	if (!new)
 		return (NULL);
-	new->str = strdup(st);
+	new->str = strdup(str);
 	new->len = len;
 	new->next = (*head);
 	(*head) = new;
